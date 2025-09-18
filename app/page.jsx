6f3e0df51b093315b2a502b50063ae78966d2b0a
Jayden Jacky <184793126+Jayden-j1@users.noreply.github.com>
2025-09-18 12:39:30 +1000

    Acknowledgement of Country component and added to the home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,55 @@
 import Section1 from "@/components/Main/section1";
 import Section2 from "@/components/Main/section2";
 import TopofPageContent from "../components/topPage/topOfPageStyle";
+import { useState } from "react";
+import AcknowledgementOfCountry from "../components/Country/acknowledgementOfCountry";
 
 
 
-export default function Home() {
+export default function Home( {} ) {
+
+  // Message for Acknowleddgement of Country
+  const message = "We acknowledge the Traditional Custodians of the lands on which we work and live. We pay our respects to Elders past and present and extend that respect to all First Nations peoples.";
+
+  // State to control the modals visibility
+  const [isModalOpen, setModalOpen] = useState();
+
+  
+  // Function to open the modal
+  const openAcknowledge = (e) => {
+    // PreventDefault prenets the link from navigating else where
+    e.preventDefault();  
+    // Show the modal
+    setModalOpen(true);    ;
+  }
+
+
+  // Function to close the modal
+  // Hide the modal
+  const closeAcknowledgement = () => setModalOpen(false); 
+
+
   return (
     <>
+      {/* Top of page content,including Acknowledgement of Country link */}
       <TopofPageContent
         HeadingOneTitle="Nyanbul Cultural Awareness"
         paragraphContent="Cultural Awareness training from the Grass-Roots Community"
+        linkOne="Acknowledgement of Country" 
+        
+        // Pass the open handler to the link 
+        onClick={openAcknowledge}
       />
+
+
+      {/* Moda; Component (pop up message) - Only shows if isModalOpen is true */}
+      <AcknowledgementOfCountry 
+        isOpen={isModalOpen}
+        onClose={closeAcknowledgement}
+        message={message}
+      />
+
+
       <Section1 className="mt-40" />
       <Section2 />
     </>
